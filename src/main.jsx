2c1f3bc09c385a51373634d1fb5119568b12c6aa
nlaghmiri1 <nlaghmiri86@gmail.com>
2025-12-14 00:53:50 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import Login from "./Login";
+import Dashboard from "./Dashboard";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const path = window.location.pathname;
+
+let Page = App;
+if (path === "/login") Page = Login;
+if (path === "/dashboard") Page = Dashboard;
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Page />
   </React.StrictMode>
 );
